943c4a4f77fa12261fd6b95c670b428b789e8c9b
Sudhakar <sudhakar.athikari@hpe.com>
2016-09-26 03:38:16 -0700

    chg: dev: Adding radius and tacacs source-interface cli support
    Change-Id: Ib564cb4164536bac7f809f0619a06d212907acf5
    Signed-off-by: Sudhakar <sudhakar.athikari@hpe.com>

--- ops-tests/component/test_vtysh_ct_source_interface_selection.py
+++ ops-tests/component/test_vtysh_ct_source_interface_selection.py
@@ -86,7 +86,7 @@ def test_source_interface_configuration(topology, step):
 
     sw1("ip source-interface all 1.1.1.1")
     out = sw1("do show ip source-interface")
-    assert '1.1.1.1' in out
+    assert 'tftp            1.1.1.1' in out
 
     step('Set source IP address to tftp protocol')
     sw1("interface 2")
@@ -94,18 +94,44 @@ def test_source_interface_configuration(topology, step):
     sw1("exit")
     sw1("ip source-interface tftp 2.2.2.2")
     out = sw1("do show ip source-interface tftp")
-    assert '2.2.2.2' in out
+    assert 'tftp            2.2.2.2' in out
+
+    step('Set source IP address to tacacs protocol')
+    sw1("interface 3")
+    sw1("ip address 3.3.3.3/24")
+    sw1("exit")
+    sw1("ip source-interface tacacs 3.3.3.3")
+    out = sw1("do show ip source-interface tacacs")
+    assert 'tacacs            3.3.3.3' in out
+
+    step('Set source IP address to radius protocol')
+    sw1("interface 4")
+    sw1("ip address 4.4.4.4/24")
+    sw1("exit")
+    sw1("ip source-interface radius 4.4.4.4")
+    out = sw1("do show ip source-interface radius")
+    assert 'radius            4.4.4.4' in out
 
     step('Set source IP interface to tftp protocol')
     sw1("ip source-interface tftp interface 1")
     out = sw1("do show ip source-interface tftp")
-    assert '1' in out
+    assert 'tftp            1' in out
+
+    step('Set source IP interface to tacacs protocol')
+    sw1("ip source-interface tacacs interface 1")
+    out = sw1("do show ip source-interface tacacs")
+    assert 'tacacs            1' in out
+
+    step('Set source IP interface to radius protocol')
+    sw1("ip source-interface radius interface 1")
+    out = sw1("do show ip source-interface radius")
+    assert 'radius            1' in out
 
     step('Set source IP interface to all the defined protocols')
     sw1("no ip source-interface tftp")
     sw1("ip source-interface all interface 2")
     out = sw1("do show ip source-interface")
-    assert '2' in out
+    assert 'tftp            2' in out
 
     step('Unset source IP to all the defined protocols')
     sw1("no ip source-interface all")
@@ -118,10 +144,32 @@ def test_source_interface_configuration(topology, step):
     out = sw1("do show ip source-interface tftp")
     assert '' in out
 
+    step('Unset source IP to tacacs protocols')
+    sw1("ip source-interface tacacs 1.1.1.1")
+    sw1("no ip source-interface tacacs")
+    out = sw1("do show ip source-interface tacacs")
+    assert '' in out
+
+    step('Unset source IP to radius protocols')
+    sw1("ip source-interface radius 1.1.1.1")
+    sw1("no ip source-interface radius")
+    out = sw1("do show ip source-interface radius")
+    assert '' in out
+
     step('show running config of source-interface to tftp protocol')
     sw1("ip source-interface tftp 2.2.2.2")
     out = sw1("do show running-config")
     assert 'ip source-interface tftp 2.2.2.2' in out
+
+    step('show running config of source-interface to tacacs protocol')
+    sw1("ip source-interface tacacs 2.2.2.2")
+    out = sw1("do show running-config")
+    assert 'ip source-interface tacacs 2.2.2.2' in out
+
+    step('show running config of source-interface to radius protocol')
+    sw1("ip source-interface radius 2.2.2.2")
+    out = sw1("do show running-config")
+    assert 'ip source-interface radius 2.2.2.2' in out
 
     step('show running config of source-interface to all the defined protocols')
     sw1("ip source-interface all interface 2")
